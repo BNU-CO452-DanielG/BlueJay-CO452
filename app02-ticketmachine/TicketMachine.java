39cdf602f40d215d5baaf8bf37b7b0ab02a1eea6
Daniel Grace <daniel.grace200011@gmail.com>
2020-10-20 14:59:14 +0100

    Changed Ticket Machine Class
    Made some adjustments to the ticket machine class and the layout of prints.

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -9,29 +9,37 @@
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  * 
- * Modified by Daniel Grace
+ * Modified by Student Name
  */
 public class TicketMachine
 {
-    // The price of a ticket from this machine.
-    private int price;
+    //The method for using coins for payment.
+    public Coin coin;
+
     // The amount of money entered by a customer so far.
     private int balance;
+    
+    // The total amount of money collected by this machine.
+    private int total;
+    
+    //Creates a new ticket, inserting the destination and the price.
+    public Ticket aylesburyTicket = new Ticket("Aylesbury", 220);
+    public Ticket amershamTicket = new Ticket("Amersham", 300);
+    public Ticket highWycombeTicket = new Ticket("High Wycombe", 330);
+
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
-        price = cost;
         balance = 0;
     }
-
-    /**
-     * @Return The price of a ticket.
-     */
-    public int getPrice()
+    
+    public void addCoin(Coin coin)
     {
-        return price;
+        System.out.println("You have inserted: " + coin);
+        balance = balance + coin.getValue();
+        System.out.println("Your balance is: " + balance + "p");
     }
 
     /**
@@ -43,23 +51,18 @@ public class TicketMachine
         return balance;
     }
 
-    /**
-     * Receive an amount of money from a customer.
-     * Check that the amount is sensible.
-     */
-    public void insertMoney(int amount)
+    public void printAllTickets()
     {
-        if(amount > 0) 
-        {
-            balance = balance + amount;
-        }
-        else 
-        {
-            System.out.println("Use a positive amount rather than: " +
-                               amount);
-        }
+        System.out.println("Our avaliable tickets are: ");
+        System.out.println("                           ");
+        System.out.println("The Amersham ticket: ");
+        amershamTicket.PrintTicketInfo();
+        System.out.println("The Aylesbury ticket: ");
+        aylesburyTicket.PrintTicketInfo();
+        System.out.println("The High Wycombe ticket: ");
+        highWycombeTicket.PrintTicketInfo();
     }
-
+    
     /**
      * Print a ticket if enough money has been inserted, and
      * reduce the current balance by the ticket price. Print
@@ -67,36 +70,17 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance >= price) 
-        {
-            // Simulate the printing of a ticket.
-            System.out.println("##################");
-            System.out.println("# The BlueJ Line");
-            System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
-            System.out.println("##################");
-            System.out.println();
-            
-            // Reduce the balance by the price.
-            balance = balance - price;
-        }
-        else 
-        {
-            System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
-                    
-        }
+        
+        
     }
 
     /**
      * Return the money in the balance.
      * The balance is cleared.
      */
-    public int refundBalance()
+    public void refundBalance()
     {
-        int amountToRefund;
-        amountToRefund = balance;
+        System.out.println("You have been refunded: " + balance + "p");
         balance = 0;
-        return amountToRefund;
     }
 }
